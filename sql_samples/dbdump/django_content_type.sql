c5c6467a9aedd6950657d972dadbd84bde7ba25d
Christopher <christopher.hale.pearce@gmail.com>
2017-02-24 00:13:32 -0500

    db is populated with data, dbdump directory created so we can restore db if needed

---
+++ sql_samples/dbdump/django_content_type.sql
@@ -0,0 +1,9 @@
+INSERT INTO django_content_type (id, app_label, model) VALUES (1, 'admin', 'logentry');
+INSERT INTO django_content_type (id, app_label, model) VALUES (2, 'auth', 'permission');
+INSERT INTO django_content_type (id, app_label, model) VALUES (3, 'auth', 'group');
+INSERT INTO django_content_type (id, app_label, model) VALUES (4, 'auth', 'user');
+INSERT INTO django_content_type (id, app_label, model) VALUES (5, 'contenttypes', 'contenttype');
+INSERT INTO django_content_type (id, app_label, model) VALUES (6, 'sessions', 'session');
+INSERT INTO django_content_type (id, app_label, model) VALUES (7, 'polls', 'choice');
+INSERT INTO django_content_type (id, app_label, model) VALUES (8, 'polls', 'question');
+INSERT INTO django_content_type (id, app_label, model) VALUES (9, 'registrar_analyzer', 'courses');
---
+++ sql_samples/dbdump/sqlite_sequence.sql
@@ -0,0 +1,7 @@
+INSERT INTO sqlite_sequence (name, seq) VALUES ('django_migrations', 15);
+INSERT INTO sqlite_sequence (name, seq) VALUES ('django_admin_log', 0);
+INSERT INTO sqlite_sequence (name, seq) VALUES ('django_content_type', 9);
+INSERT INTO sqlite_sequence (name, seq) VALUES ('auth_permission', 27);
+INSERT INTO sqlite_sequence (name, seq) VALUES ('auth_user', 0);
+INSERT INTO sqlite_sequence (name, seq) VALUES ('polls_choice', 0);
+INSERT INTO sqlite_sequence (name, seq) VALUES ('registrar_analyzer_courses', 964);
