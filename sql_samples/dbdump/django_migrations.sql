afaa8d5c749d62f791e9d1fe76bf3e692677d1e3
Christopher <christopher.hale.pearce@gmail.com>
2017-02-24 20:20:00 -0500

    db is populated with data, dbdump directory created so we can restore db if needed

---
+++ sql_samples/dbdump/django_migrations.sql
@@ -0,0 +1,15 @@
+INSERT INTO django_migrations (id, app, name, applied) VALUES (1, 'contenttypes', '0001_initial', '2017-02-15 14:53:30.297302');
+INSERT INTO django_migrations (id, app, name, applied) VALUES (2, 'auth', '0001_initial', '2017-02-15 14:53:30.331664');
+INSERT INTO django_migrations (id, app, name, applied) VALUES (3, 'admin', '0001_initial', '2017-02-15 14:53:30.352948');
+INSERT INTO django_migrations (id, app, name, applied) VALUES (4, 'admin', '0002_logentry_remove_auto_add', '2017-02-15 14:53:30.384590');
+INSERT INTO django_migrations (id, app, name, applied) VALUES (5, 'contenttypes', '0002_remove_content_type_name', '2017-02-15 14:53:30.429383');
+INSERT INTO django_migrations (id, app, name, applied) VALUES (6, 'auth', '0002_alter_permission_name_max_length', '2017-02-15 14:53:30.447841');
+INSERT INTO django_migrations (id, app, name, applied) VALUES (7, 'auth', '0003_alter_user_email_max_length', '2017-02-15 14:53:30.469515');
+INSERT INTO django_migrations (id, app, name, applied) VALUES (8, 'auth', '0004_alter_user_username_opts', '2017-02-15 14:53:30.497490');
+INSERT INTO django_migrations (id, app, name, applied) VALUES (9, 'auth', '0005_alter_user_last_login_null', '2017-02-15 14:53:30.533273');
+INSERT INTO django_migrations (id, app, name, applied) VALUES (10, 'auth', '0006_require_contenttypes_0002', '2017-02-15 14:53:30.538508');
+INSERT INTO django_migrations (id, app, name, applied) VALUES (11, 'auth', '0007_alter_validators_add_error_messages', '2017-02-15 14:53:30.563997');
+INSERT INTO django_migrations (id, app, name, applied) VALUES (12, 'auth', '0008_alter_user_username_max_length', '2017-02-15 14:53:30.589293');
+INSERT INTO django_migrations (id, app, name, applied) VALUES (13, 'polls', '0001_initial', '2017-02-15 14:53:30.609181');
+INSERT INTO django_migrations (id, app, name, applied) VALUES (14, 'registrar_analyzer', '0001_initial', '2017-02-15 14:53:30.630979');
+INSERT INTO django_migrations (id, app, name, applied) VALUES (15, 'sessions', '0001_initial', '2017-02-15 14:53:30.641727');
