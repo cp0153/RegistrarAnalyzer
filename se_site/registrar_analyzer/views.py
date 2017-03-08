32519e640a9c9aefc4d40bf93dc5c308ef0fd050
Dossar <vanliewroy@gmail.com>
2017-03-08 19:43:40 +0000

    Alright, so the form reappears after the post request.

--- se_site/registrar_analyzer/views.py
+++ se_site/registrar_analyzer/views.py
@@ -6,6 +6,7 @@ from django.views import generic
 from .models import Courses
 
 from python_scripts.terminfo import *
+course_options_array = [orderedSemesterList, sortedFullCourseNames]
 
 # Create your views here.
 class IndexView(generic.ListView):
@@ -13,8 +14,10 @@ class IndexView(generic.ListView):
     template_name = 'registrar_analyzer/index.html'
     context_object_name = 'course_options_array' # Context variable
 
+
     def post(self, request, *args, **kwargs):
         postInfo = {}
+
         #if request.method == "POST":
         courseName = request.POST['courseNameSelect']
         startSemester = request.POST['startSemesterSelect']
@@ -24,12 +27,12 @@ class IndexView(generic.ListView):
         postInfo['startSemesterSelect'] = startSemester
         postInfo['endSemesterSelect'] = endSemester
         postInfo['graphSelect'] = graphType
-        return render(request, self.template_name, {'postInfo': postInfo})
+        return render(request, self.template_name, {'postInfo': postInfo, 'course_options_array': course_options_array})
         #return [orderedSemesterList, sortedFullCourseNames, postInfo]
 
     def get_queryset(self):
         """ Get the last 5 course info """
-        return [orderedSemesterList, sortedFullCourseNames]
+        return course_options_array
 
 """
 def index(request):
