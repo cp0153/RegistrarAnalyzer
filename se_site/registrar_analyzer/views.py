392e187bb0d52fb23ebebce6b27ee671b1fc698c
Dossar <vanliewroy@gmail.com>
2017-03-08 21:25:12 +0000

    POST no longer makes a blank page

--- se_site/registrar_analyzer/views.py
+++ se_site/registrar_analyzer/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect, HttpResponse, HttpRequest
 from django.urls import reverse
-from django.views import generic
+from django.views import generic, View
 
 from .models import Courses
 
@@ -9,11 +9,14 @@ from python_scripts.terminfo import *
 course_options_array = [orderedSemesterList, sortedFullCourseNames]
 
 # Create your views here.
-class IndexView(generic.ListView):
+class IndexView(View):
 
     template_name = 'registrar_analyzer/index.html'
     context_object_name = 'course_options_array' # Context variable
 
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name,
+                      {'course_options_array': course_options_array})
 
     def post(self, request, *args, **kwargs):
         postInfo = {}
@@ -33,10 +36,6 @@ class IndexView(generic.ListView):
             return render(request, self.template_name,
                           {'course_options_array': course_options_array})
 
-
-    def get_queryset(self):
-        """ Get the last 5 course info """
-        return course_options_array
 
 """
 def index(request):
