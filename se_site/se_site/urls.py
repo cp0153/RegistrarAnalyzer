56f780c7160b68ce7f3f0c980dc1f1502c1af91a
Christopher <christopher.hale.pearce@gmail.com>
2017-03-24 21:07:59 -0400

    removed static is urls.py

--- se_site/se_site/urls.py
+++ se_site/se_site/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^static/', include('static.urls')),
+    url(r'^polls/', include('polls.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^ra/', include('registrar_analyzer.urls')),
 ]
